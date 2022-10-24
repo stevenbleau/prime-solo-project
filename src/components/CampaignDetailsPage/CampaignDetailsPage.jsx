edf46d570050fd541d3153037c37ac2dd11c2a65
Steven Bleau <bleausd@gmail.com>
2022-10-24 15:09:20 -0500

    campaign details page fixed

--- src/components/CampaignDetailsPage/CampaignDetailsPage.jsx
+++ src/components/CampaignDetailsPage/CampaignDetailsPage.jsx
@@ -8,38 +8,22 @@ import { useParams } from 'react-router-dom';
 
 function CampaignDetailsPage() {
 
-  const dispatch = useDispatch();
-  // let [campaign, setCampaign] = useState([]);
+  let [campaignDetails, setCampaignDetails] = useState([]);
   const [itemList, setItemList] = useState([]);
-  const user = useSelector(store => store.user)
-  const campaign = useSelector(store => store.campaign)
   const {id} = useParams();
 
+  //ORIGINALLY CALLED TO TEST REDUCER
+  // const campaign = useSelector(store => store.campaign)
 
+  // GET REQUEST ORIGINALLY USED TO GET CAMPAIGN DETAILS BEFORE USING REDUCER
 
-// GET REQUEST ORIGINALLY USED TO GET CAMPAIGN DETAILS BEFORE USING REDUCER
-  // const fetchCampaign = () => {
-  //   axios({
-  //       method: 'GET',
-  //       url: `/api/campaign/${id}`
-  //   }).then (response => {
-  //     console.log('the capaign response.data is ', response.data);
-  //     setCampaign(response.data);
-  //   }).catch (error => {
-  //     console.log('error in fetchCampaigns')
-  //     console.log(error);
-  //     alert('Something went wrong!')
-  //   })
-  // }
-
-  const fetchItems = () => {
+  const fetchCampaign = () => {
     axios({
         method: 'GET',
-        url: `/api/campaign/item/${id}`
+        url: `/api/campaign/${id}`
     }).then (response => {
-      console.log('the fetchItems response.data is ', response.data);
-      setItemList(response.data);
-      console.log('campaign details are', campaign.campaign.title);
+      console.log('the campaign response.data is ', response.data);
+      setCampaignDetails(response.data);
     }).catch (error => {
       console.log('error in fetchCampaigns')
       console.log(error);
@@ -48,8 +32,24 @@ function CampaignDetailsPage() {
   }
 
 
+
+  const fetchItems = () => {
+    axios({
+        method: 'GET',
+        url: `/api/campaign/item/${id}`
+    }).then (response => {
+      console.log('the fetchItems response.data is ', response.data);
+      setItemList(response.data);
+    }).catch (error => {
+      console.log('error in fetchItems')
+      console.log(error);
+      alert('Something went wrong!')
+    })
+  }
+
+
   useEffect(() => {
-    // fetchCampaign();
+    fetchCampaign();
     fetchItems();
   }, []);
 
@@ -58,36 +58,36 @@ function CampaignDetailsPage() {
   return (
     <div className="container">
       
+      <ul>
+        {campaignDetails.map(campaign => {
+          return(
+            <div key={campaign.id}>
+              <img src={campaign.campaign_image_url}></img>
+              <h1>{campaign.title}</h1>
+              <h3>{campaign.description}</h3>
+            </div>
+          );
+        })}
+      </ul>
 
 
-        
-        <img src={campaign.campaign.campaign_image_url} />
+  {/* Used to test Campaign Detail Reducer */}
+        {/* <img src={campaign.campaign.campaign_image_url} />
         <h2>{campaign.campaign.title}</h2>
         <h5>{campaign.campaign.location}</h5>
-        <h3>{campaign.campaign.description}</h3>
+        <h3>{campaign.campaign.description}</h3> */}
 
       <ul>
           {itemList.map(item => {
             return (
               <div key={item.item_id}>
-                <h5>{item.item_name} {item.item_quantity}</h5>
+                <h5>{item.item_name} {item.pledge_count}/{item.item_quantity}</h5>
                 <h5>{item.item_description}</h5>
                 <button>Pitch In</button>
               </div>
             );
           })}
       </ul>
-
-      {/* <ul>
-        {campaignArray.map(campaign => {
-          return 
-          <div>
-            <h5>{campaign.item_name} {campaign.item_quantity}</h5>
-            <h5>{campaign.item_description}</h5>
-            <button>Pitch In</button>
-          </div>    
-        })}
-      </ul> */}
       
     </div>
   );
