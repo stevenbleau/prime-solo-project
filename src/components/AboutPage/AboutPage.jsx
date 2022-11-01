7fd4961a135df263af16be6d8bc15a44bc181fd4
Steven Bleau <bleausd@gmail.com>
2022-11-01 14:49:35 -0500

    about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,30 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <h1>Thank You!</h1>
+        <ul>
+          <li>Prime Academy</li>
+          <li>Chris Black</li>
+          <li>Monday pod groupmates</li>
+        </ul>
+        <h3>Technologies Used</h3>
+        <ul>
+          <li>React JS</li>
+          <li>Node/Express</li>
+          <li>Postgresql</li>
+        </ul>
+        <h3>Challenges</h3>
+        <ul>
+          <li>SQL relational databases</li>
+          <li>File Management/Organization</li>
+        </ul>
+
+        <h3>Next Steps</h3>
+        <ul>
+          <li>Websocket Messaging</li>
+          <li>Image Uploads with AWS s3</li>
+          <li>Heroku Deployment</li>
+        </ul>
       </div>
     </div>
   );
